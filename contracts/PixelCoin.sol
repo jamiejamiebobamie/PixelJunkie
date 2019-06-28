38ed9356430436ddce2ca04bd1f3035343917ca6
jamiejamiebobamie <jmccrory@vt.edu>
2019-06-27 17:04:56 -0700

    wrote a function to add new pictures to the array of pictures

--- contracts/PixelCoin.sol
+++ contracts/PixelCoin.sol
@@ -7,8 +7,8 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 
 contract PixelCoin is ERC721Full, ERC721Mintable {
 
-    address public owner;
-    uint public last_completed_migration;
+  address public owner;
+  uint public last_completed_migration;
   constructor() ERC721Full("PixelCoin", "PixelCoin") public {
         owner = msg.sender;
   }
@@ -39,15 +39,18 @@ contract PixelCoin is ERC721Full, ERC721Mintable {
       string picture; // which picture the pixel is part of.
   }
 
-      // Pixel array called pixels
-      Pixel[] public pixels;
+    // Picture-name array
+    string[] public pictures;
 
-      mapping (uint => address) public pixelToOwner;
-      mapping (address => uint) ownerPixelCount;
+    // Pixel array called pixels
+    Pixel[] public pixels;
 
-      // key: an address
-      // value: [pixelsBoughtOnADay, aDay'sDate]
-      mapping (address => uint[]) ownertoPixelsBoughtAndBoughtDay;
+    mapping (uint => address) public pixelToOwner;
+    mapping (address => uint) ownerPixelCount;
+
+    // key: an address
+    // value: [pixelsBoughtOnADay, aDay'sDate]
+    mapping (address => uint[]) ownertoPixelsBoughtAndBoughtDay;
 
 
     //NOT WRITTEN-----
@@ -59,7 +62,6 @@ contract PixelCoin is ERC721Full, ERC721Mintable {
         }
     //NOT WRITTEN-----
 
-
     function createPixel(uint16[2] _xy, string _picture) internal {
         uint id = pixels.push(Pixel(_xy, _picture)) - 1;
         pixelToOwner[id] = msg.sender;
@@ -67,32 +69,32 @@ contract PixelCoin is ERC721Full, ERC721Mintable {
         emit NewPixelRevealed(_xy, _picture);
     }
 
+  function buyUnownedPixel(uint16[2] _xy, string _picture) public {
+      // require that the amount of pixels bought today is less than 3
+      // or require that today's date is different than the date stored from the last buy.
+      require(ownerPixelsBoughtPerDay[msg.sender][0] < 3 || ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now));
 
-      function buyUnownedPixel(_xy, _picture) public {
-          // require that the amount of pixels bought today is less than 3
-          // or require that today's date is different than the date stored from the last buy.
-          require(ownerPixelsBoughtPerDay[msg.sender][0] < 3 || ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now));
+      // create a new pixel.
+      createPixel(_xy, _picture);
 
-          createPixel(_xy, _picture);
-
-          // if it hasn't been one day since the last purchase
-          if (ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now)){
-              //increment the amount of pixels bought for that day
-              ownerPixelsBoughtPerDay[msg.sender][0]++; //SAFE MATH!!!
-          } else {
-              //else set the time to the new day
-              ownerPixelsBoughtPerDay[msg.sender][1] = now;
-              //and increment the number of purchases for that day to 1
-              ownerPixelsBoughtPerDay[msg.sender][0] = 1;
-          }
-
+      // if it hasn't been one day since the last purchase
+      if (ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now)){
+          //increment the amount of pixels bought for that day
+          ownerPixelsBoughtPerDay[msg.sender][0]++; //SAFE MATH!!!
+      } else {
+          //else set the time to the new day
+          ownerPixelsBoughtPerDay[msg.sender][1] = now;
+          //and increment the number of purchases for that day to 1
+          ownerPixelsBoughtPerDay[msg.sender][0] = 1;
       }
+
+  }
 
 // return all pixels owned by the caller of the function
     function showPixels() public view returns (pixels){
          Pixel[] public ownersPixels;
          for (uint i = 0; i < pixels.length(); i++){
-             if (pixels[i] == msg.sender){
+             if (pixelToOwner[i] == msg.sender){
                  ownersPixels.push(pixels[i])
              }
          }
@@ -100,9 +102,21 @@ contract PixelCoin is ERC721Full, ERC721Mintable {
     }
 
 // STRETCH CHALLENGE: offerToBuy function:: offer to buy the pixels from the owner
-    function offerToBuyOwnedPixel(address _buyer, address _seller) external {
+    function offerToBuyOwnedPixel(address _buyer, address _seller, uint16[2] _xy, string _picture) external {
 
     }
 
-
+// A function to add new names to the array of picture names
+    function addPictureToArrayOfPictures(string _name) public restricted {
+        bool unique = true;
+        // checks to make sure the name doesn't already exist in the array
+        for (uint i = 0; i < pictures.length(); i++){
+            if (pictures[i] == _name){
+                unique = false;
+            }
+        // if it doesn't,
+        require(unique == true);
+        // add it to the array.
+        pictures.push(_name);
+    }
 }
