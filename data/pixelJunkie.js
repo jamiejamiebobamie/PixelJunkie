c1428a3ab3cd45f40a59051bfa20621be24d84f6
jamiejamiebobamie <jmccrory@vt.edu>
2019-06-30 16:10:10 -0700

    changed my database to the heroku one

--- data/pixelJunkie.js
+++ data/pixelJunkie.js
@@ -2,10 +2,16 @@
 const mongoose = require("mongoose");
 assert = require("assert");
 
-const url = "mongodb://localhost/pixelJunkie";
+// //heroku database.
+// mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/database'), { useNewUrlParser: true });
+
+// local host database
+// mongoose.connect('mongodb://localhost/pixelJunkie');
+
+const url = "mongodb://localhost/database";
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  "mongodb://localhost/pixelJunkie",
+  "mongodb://localhost/database",
   { useNewUrlParser: true }
 );
 mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
