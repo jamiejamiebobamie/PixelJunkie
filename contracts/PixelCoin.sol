8f0e0e9a0a9e5c7d16cf502d2fbed0a330734012
jamiejamiebobamie <jmccrory@vt.edu>
2019-06-30 11:31:05 -0700

    added more to buyUnownedPixels function

--- contracts/PixelCoin.sol
+++ contracts/PixelCoin.sol
@@ -25,41 +25,45 @@ contract PixelCoin is ERC721Full, ERC721Mintable {
     Migrations upgraded = Migrations(new_address);
     upgraded.setCompleted(last_completed_migration);
   }
-/* Untested, foreign, scary. */
-
-
-
-
+/* ^Untested, foreign, scary. */
 
     event NewPixelRevealed(uint16[2] _xy, string _picture, string _hexCode);
 
-    uint sizeOfPicture = 600; // length and width of picture
+    uint sizeOfPicture = 600; // length and width of picture length x width.
 
+    // THE "COINAGE" --> PIXELS
     struct Pixel {
       uint16[2] xy; // the [x,y] coordinate of the pixel, acts as the pixel's _id
       string picture; // which picture the pixel is part of.
     }
-    // Picture-name array
-    string[] public pictures;
-    // Pixel array called pixels
-    Pixel[] public pixels;
 
-    mapping (uint => address) public pixelToOwner;
-    mapping (address => uint) ownerPixelCount;
+    // Picture-name array
+    // USED TO STORE ALL THE NAMES OF THE PICTURES
+    string[] public pictures;
+
+    // picture's name to pixel-struct-array mapping
+    // USED TO STORE ALL THE AVAILABLE PIXEL COORDINATES ASSOCIATED WITH A GIVEN PICTURE
+    mapping (string => uint[]) public pictureToPixels;
+
+    // USED TO STORE THE OWNED PIXELS OF A GIVEN USER
+    mapping (address => Pixel[]) public ownerToPixels;
 
     // key: an address
     // value: [pixelsBoughtOnADay, aDay'sDate]
-    mapping (address => uint[]) ownertoPixelsBoughtAndBoughtDay;
+    // USED TO CONTROL THE USERS' BUY RATE
+    mapping (address => uint[]) ownertoBoughtCountAndBoughtDay;
 
-
-    //NOT WRITTEN-----
+    //**//NOT WRITTEN-----
     //generate random pixel to buy from unowned pixels
-      function _generateRandomPixel(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
-        // dnaModulus
-        return rand % sizeOfPicture;
+      function _generateRandomPixel(string _str) private view returns (uint[]) {
+          result = []
+        uint rand1 = uint(keccak256(abi.encodePacked(_str)));
+        uint rand2 = uint(keccak256(abi.encodePacked(_str)));
+        result.push(rand1 % sizeOfPicture);
+        result.push(rand2 % sizeOfPicture);
+        return result
         }
-    //NOT WRITTEN-----
+    //**//NOT WRITTEN-----
 
     function createPixel(uint16[2] _xy, string _picture) internal {
         uint id = pixels.push(Pixel(_xy, _picture)) - 1;
@@ -69,24 +73,43 @@ contract PixelCoin is ERC721Full, ERC721Mintable {
     }
 
     function buyUnownedPixel(uint16[2] _xy, string _picture) public {
-    // require that the amount of pixels bought today is less than 3
-    // or require that today's date is different than the date stored from the last buy.
-    require(ownerPixelsBoughtPerDay[msg.sender][0] < 3 || ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now));
+        // require that the amount of pixels bought today is less than 3
+        // or require that today's date is different than the date stored from the last buy.
+        require(ownerPixelsBoughtPerDay[msg.sender][0] < 3 || ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now));
 
-    // create a new pixel.
-    createPixel(_xy, _picture);
+        // require that there are still pixels left to purchase from the given picture
+        require(pictureToPixels[_picture].length != 0);
 
-    // if it hasn't been one day since the last purchase
-    if (ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now)){
-        //increment the amount of pixels bought for that day
-        ownerPixelsBoughtPerDay[msg.sender][0]++; //SAFE MATH!!!
-    } else {
-        //else set the time to the new day
-        ownerPixelsBoughtPerDay[msg.sender][1] = now;
-        //and increment the number of purchases for that day to 1
-        ownerPixelsBoughtPerDay[msg.sender][0] = 1;
-    }
+    //**//NOT WRITTEN-----
+        //NEED TO GENERATE A RANDOM PIXEL IN AN EFFICIENT WAY FROM THE PIXELS STILL AVAILABLE
+        // _generateRandomPixel(string _str)
+    //**//NOT WRITTEN-----
 
+        uint randCoordinate; // the coordinate randomly generated
+
+        for (uint i = 0; i < pictureToPixels[_picture].length; i++) {
+            // find the item in the array that is equal to the random coordinate generated.
+            if (pictureToPixels[_picture][i] == randCoordinate) {
+                // duplicate the last entry in the array into the index that stores the random coordinate
+                // pop the last entry.
+                pictureToPixels[_picture][i] = pictureToPixels[_picture][-i];
+                pictureToPixels[_picture].pop();
+            }
+        }
+
+        // create a new pixel and push it to the ownerToPixel array.
+        ownerToPixels[msg.sender].push(createPixel(_xy, _picture));
+
+        // if it hasn't been one day since the last purchase
+        if (ownerPixelsBoughtPerDay[msg.sender][1] + 1 days < now)){
+            //increment the amount of pixels bought for that day
+            ownerPixelsBoughtPerDay[msg.sender][0]++; //SAFE MATH!!!
+        } else {
+            //else set the time to the new day
+            ownerPixelsBoughtPerDay[msg.sender][1] = now;
+            //and increment the number of purchases for that day to 1
+            ownerPixelsBoughtPerDay[msg.sender][0] = 1;
+        }
     }
 
     // return all pixels owned by the caller of the function
@@ -96,10 +119,10 @@ contract PixelCoin is ERC721Full, ERC721Mintable {
         for (uint i = 0; i < pixels.length(); i++){
          // use the index as the lookup to find the owner of the pixel
          // if the owner is the caller of the function
-         if (pixelToOwner[i] == msg.sender){
-             // push the pixels into the array to be returned by the function
-             ownersPixels.push(pixels[i])
-         }
+             if (pixelToOwner[i] == msg.sender){
+                 // push the pixels into the array to be returned by the function
+                 ownersPixels.push(pixels[i])
+             }
         }
         return ownersPixels
     }
